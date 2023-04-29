34a813796aa78551ddd30521402274c36d95b5de
DELL <kolloju.aishwarya@gmail.com>
2023-04-29 14:03:27 +0530

    < information checks>

---
+++ 01.variables/var.tf
@@ -0,0 +1,7 @@
+variable "sample_string" {
+  default = "hello world"
+}
+#shell scripting Sample_string = "hello world"
+#to print in shell script we use echo.
+# echo sample_string
+
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # learn-terraform
 
-## 1.Variables
+1.Variables
+2.data source
+3.resource
+4.module
+5.locals
+6.provider
+7.state files
+8.loops
+9.conditions
+
