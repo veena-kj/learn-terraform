fc56a3a4db2aac3a8cc5b34bdc02c3a8358b5c48
DELL <kolloju.aishwarya@gmail.com>
2023-05-02 19:05:10 +0530

    < Security group all details and ID check >

--- 02-data-sources/main.tf
+++ 02-data-sources/main.tf
@@ -1,6 +1,9 @@
 data "aws_security_group" "check" {
   name = "allow-all"
 }
-output "security_grp_ID" {
+output "security_grp_details" {
   value = data.aws_security_group.check
+}
+output "sec_grp_ID" {
+  value=data.aws_security_group.check.id
 }
