d982d46765ef225441959e70736abfce326a9359
DELL <kolloju.aishwarya@gmail.com>
2023-05-03 00:03:41 +0530

    < for_each accessing way for nested>

--- 03-loops/for_each/main.tf
+++ 03-loops/for_each/main.tf
@@ -13,3 +13,28 @@ resource "null_resource" "fruits" {
     command = "echo Fruits available are ${each.key} - ${each.value}"
    }
 }
+
+variable "fruits1" {
+  default = {
+    watermelon ={
+      name = "watermelon"
+      count = 82
+    }
+    Strawberries ={
+      name ="Strawberries"
+      count = 75
+    }
+    sweetpotato = {
+      name = "sweet-potato"
+      count = 230
+    }
+  }
+
+}
+resource "null_resource" "fruits1" {
+  for_each = var.fruits1
+
+  provisioner "local-exec" {
+    command = "echo fruite1 are available as below ${each.value[name]} and ${each.value[count]}"
+  }
+}
