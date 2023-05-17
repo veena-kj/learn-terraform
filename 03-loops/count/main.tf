ca22ef1eb27a5f9983901865d32fe27deca0a2dd
DELL <kolloju.aishwarya@gmail.com>
2023-05-17 16:59:42 +0530

    <null resource-length and count with provisioner local exec example>

--- 03-loops/count/main.tf
+++ 03-loops/count/main.tf
@@ -3,6 +3,10 @@ resource "null_resource" "null" {
 }
 resource "null_resource" "fruits" {
   count = length(var.fruits)
+
+  provisioner "local-exec" {
+    command = " echo fruit name is ${var.fruits(count.index)} "
+  }
 }
 
 
