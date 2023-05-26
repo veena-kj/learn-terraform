ed10e856cb5347ca0bb153fcf0e00e644e04dca3
DELL <kolloju.aishwarya@gmail.com>
2023-05-26 06:49:43 +0530

    < spell check -modules in terraform >

--- 06-module/module/main.tf
+++ 06-module/module/main.tf
@@ -1,15 +1,15 @@
 resource "null_resource" "nothing1" {
-  provioner "local-exec" {
+  provisioner "local-exec" {
     command = "echo Hello from module = $(var.input)"
   }
 }
 resource "null_resource" "nothing2" {
-  provioner "local-exec" {
+  provisioner "local-exec" {
     command = "echo Hello from module"
   }
 }
 resource "null_resource" "nothing3" {
-  provioner "local-exec" {
+  provisioner "local-exec" {
     command = "echo Hello from module"
   }
 }
